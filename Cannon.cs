d8bc96f5c9066760b5c7f6f57bd40270d0f7cb8b
Marie-Grace Ouamba <marieouamba@gmail.com>
2025-02-07 23:08:02 -0600

    Add files via upload

---
+++ Cannon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMagicCannon
+{
+    public class Cannon
+    {
+        public static void FireCannon(int turn)
+        {
+            string result = $"{turn}: ";
+
+            if (turn % 3 == 0 && turn % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                result += "Fire and Electric!";
+            }
+            else if (turn % 3 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                result += "Fire";
+            }
+            else if (turn % 5 == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                result += "Electric";
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                result += "Normal";
+            }
+
+            Console.WriteLine(result);
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TheMagicCannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Cannon.FireCannon(i);
+            }
+        }
+    }
+}
